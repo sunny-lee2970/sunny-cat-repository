b7a3bd76f153a2d3faf1a487b65cb84032d10239
sunny2970 <cj7170@naver.com>
2022-09-06 15:53:38 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import Title from "./component/Title"
+import Favorites from './component/Favorites';
+import Form from './component/Form';
+import MainCard from './component/MainCard';
 
-function App() {
+
+const jsonLocalStorage = {
+  setItem: (key, value) => {
+    localStorage.setItem(key, JSON.stringify(value));
+  },
+  getItem: (key) => {
+    return JSON.parse(localStorage.getItem(key));
+  },
+};
+
+const fetchCat = async (text) => {
+  const OPEN_API_DOMAIN = "https://cataas.com";
+  const response = await fetch(
+    `${OPEN_API_DOMAIN}/cat/says/${text}?json=true`
+  );
+  const responseJson = await response.json();
+  return `${OPEN_API_DOMAIN}/${responseJson.url}`;
+};
+
+const App = () => {
+
+  const [counter, setCounter] = React.useState(() => {
+    return jsonLocalStorage.getItem("counter");
+  });
+  const [mainCat, setMainCat] = React.useState("");
+  const [favorites, setFavorites] = React.useState(() => {
+    return jsonLocalStorage.getItem("favorites") || []
+  });
+
+  async function setInitialCat() {
+    const newCat = await fetchCat('First cat');
+    console.log(newCat);
+    setMainCat(newCat);
+  }
+
+  React.useEffect(() => {
+    setInitialCat();
+  }, []);
+
+  const alreadyFavorite = favorites.includes(mainCat); // includes는 객체안에 값이 있으면 true, 없으면 false로 판별
+
+
+
+  async function updateMainCat(value) { //await을 쓸려면 함수앞에 async를 붙혀야 한다.
+    const newCat = await fetchCat(value);
+
+    setMainCat(newCat);
+    setCounter((prev) => {
+      const nextCounter = prev + 1;
+      jsonLocalStorage.setItem("counter", nextCounter);
+      return nextCounter;
+    });
+  }
+
+  function handleHeartClick() {
+    const nextFavorites = [...favorites, mainCat];
+    setFavorites(nextFavorites);
+    jsonLocalStorage.setItem("favorites", nextFavorites);
+  }
+
+  const counterTitle = counter == null ? "" : counter + '번째';
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Title>{counterTitle} 고양이 가라사대</Title>
+      <Form updateMainCat={updateMainCat} />
+      <MainCard
+        img={mainCat}
+        onHeartClick={handleHeartClick}
+        alreadyFavorite={alreadyFavorite}
+      />
+      <Favorites favorites={favorites} />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/component/Favorites.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function CatItem(props) {
+    return (
+        <li>
+            <img src={props.img} style={{ width: "150px" }} />
+        </li>
+    );
+}
+
+function Favorites({ favorites }) {
+
+    if (favorites.length === 0) { // 조건부 랜더링
+        return <div>사진 위 하트를 눌러 고양이 사진을 저장해봐요!</div>
+    }
+    return (
+        <ul className="favorites">
+            {favorites.map((cat) => (
+                <CatItem img={cat} key={cat} />
+            ))}
+        </ul>
+    );
+}
+
+export default Favorites;
